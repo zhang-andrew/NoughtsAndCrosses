47db850886dff66a67b8ae2f302e3a88f84598ac
zhang-andrew <72960866+zhang-andrew@users.noreply.github.com>
2024-09-17 12:39:18 +1000

    Implemented solution for green test - Space.Mark Setter only assignable when Mark.Empty

--- tests/NoughtsAndCrosses.Core.Tests/BoardSpecifications.cs
+++ tests/NoughtsAndCrosses.Core.Tests/BoardSpecifications.cs
@@ -142,12 +142,28 @@ public class Board
 
 public class Space
 {
-    public Mark Mark { get; set; } = Mark.Empty;
+    private Mark _mark;
+    
     public Coordinate Coordinate { get; }
 
     public Space(Coordinate coordinate)
     {
         Coordinate = coordinate;
+        _mark = Mark.Empty;
+    }
+
+    public Mark Mark
+    {
+        get { return _mark; }
+        set
+        {
+            if (_mark != Mark.Empty)
+                throw new Exception("Space is already occupied");
+            else
+            {
+                _mark = value;
+            }
+        }
     }
 }
 
