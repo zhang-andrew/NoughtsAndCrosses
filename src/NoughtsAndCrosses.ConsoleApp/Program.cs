0f95dfe59fd9cc2c95fa49207416725d8ab8ae05
zhang-andrew <72960866+zhang-andrew@users.noreply.github.com>
2024-09-20 14:25:57 +1000

    Cleaned up comments on ConsoleApp

--- src/NoughtsAndCrosses.ConsoleApp/Program.cs
+++ src/NoughtsAndCrosses.ConsoleApp/Program.cs
@@ -1,21 +1,6 @@
 ﻿// See https://aka.ms/new-console-template for more information
 
-using NoughtsAndCrosses.Core.Service;
 using NoughtsAndCrosses.Core.Domain;
-
-Console.WriteLine("Hello, World!");
-
-// var playerClient = new PlayerClient();
-// var consoleService = new ConsoleService(playerClient);
-//
-// // Create tasks
-// var connectToWebSocket = playerClient.ConnectToWebSocket("ws://localhost:5148/ws");
-// var monitorConsoleInput = consoleService.MonitorConsoleInput();
-//
-// await Task.WhenAll(
-//     connectToWebSocket,
-//     monitorConsoleInput
-// );
 
 var gm = new GameManager();
 
@@ -25,54 +10,3 @@ AppDomain.CurrentDomain.ProcessExit += (sender, e) =>
 };
 
 gm.Run();
-
-
-//
-// static async Task ReadConsoleInput(PlayerClient client = null)
-// {
-//     while (true)
-//     {
-//         var input = Console.ReadLine();
-//         if (input == "exit")
-//         {
-//             break;
-//         }
-//         
-//         if (client != null)
-//         {
-//             await client.SendToWebSocket(input);
-//         }
-//         
-//         Console.WriteLine($"You entered: {input}");
-//         // Add logic to send input to WebSocket server if needed
-//     }
-// }
-
-//
-// var serviceProvider = new ServiceCollection()
-//     .AddSingleton<ICommandService, CommandService>() // For online play
-//     .AddSingleton<NPCService>() // For offline play
-//     .AddSingleton<GameManager>()
-//     .BuildServiceProvider();
-
-// Console.WriteLine("Choose mode: 1 for Online, 2 for Offline");
-// var choice = Console.ReadLine();
-//
-// if (choice == "1")
-// {
-//     Console.WriteLine("Connecting to server...");
-//     var playerClient = new PlayerClient();
-//     playerClient.ConnectAsync("ws://localhost:5000").Wait();
-// }
-// else
-// {
-//     Console.WriteLine("Starting offline game against NPC...");
-//     
-//     // 
-//     var npcService = serviceProvider.GetService<NPCService>();
-//     gameManager = new GameManager(npcService);
-// }
-//
-// // Additional game setup and start logic...
-// gameManager.NewGame();
-// // Example: gameManager.PlayTurn(player);
