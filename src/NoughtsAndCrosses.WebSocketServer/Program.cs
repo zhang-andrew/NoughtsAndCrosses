ddcf36d4524b6ccfbd162d90a5e5b93cde5c9c32
zhang-andrew <72960866+zhang-andrew@users.noreply.github.com>
2024-09-23 21:35:07 +1000

    Updated comment

--- src/NoughtsAndCrosses.WebSocketServer/Program.cs
+++ src/NoughtsAndCrosses.WebSocketServer/Program.cs
@@ -1,4 +1,4 @@
 using NoughtsAndCrosses.WebSocketServer.Domain;
 
 Server server = new Server();
-server.Start(args); // remember args is a global variable that is passed to the program on startup (it's the flags passed in terminal execution) 
+server.Start(args); // remember args is an argument that is passed to the program on startup (it's the flags passed in terminal execution) 
